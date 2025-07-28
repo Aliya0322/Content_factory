50e63e144faded823c38723d4e5cc977306130bd
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-07-28 18:08:38 +0300

    init commit

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 
 class Config:
-    def __init__(self, env_path=".env"):
+    def __init__(self, env_path = ".env"):
         load_dotenv(env_path)
 
         self.api_id = os.getenv("API_ID")
-        self.api_hash =os.getenv("API_HASH")
-        self.session_name = os.getenv("SESSION_NAME")
+        self.api_hash = os.getenv("API_HASH")
+        self.session_name=os.getenv("SESSION_NAME")
 
-        self.source_channels=["mosguru", "moscowmap"]
+        self.source_channels = ["mosguru", "moscowmap", "msk_live"]
